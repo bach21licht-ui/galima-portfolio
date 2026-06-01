c648e1d16821f2a3f25a062e0208041f3134f257
Brien Galima <bach21licht@gmail.com>
2026-06-01 13:43:26 +0800

    fix src submodule issue

---
+++ src/components/Achievements.jsx
@@ -0,0 +1,14 @@
+function Achievements() {
+  return (
+    <div className="section">
+      <h2>Achievements</h2>
+      <ul>
+        <li>Grade 12 – With High Honors (2022–2023)</li>
+        <li>Grade 11 – With High Honors (2021–2022)</li>
+        <li>Most Excellent Work Immersion Student</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Achievements;
